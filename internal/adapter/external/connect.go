971bf5c0dc11fa7431c4396317fc2ad9c919dda7
takokun <takokun.778@gmail.com>
2023-03-21 18:39:41 +0900

    refactor(#77) external層をリファクタリング

--- internal/adapter/external/connect.go
+++ internal/adapter/external/connect.go
@@ -4,19 +4,8 @@ import (
 	"context"
 
 	"github.com/bufbuild/connect-go"
-	"github.com/morning-night-guild/platform-app/pkg/connect/article/v1/articlev1connect"
-	"github.com/morning-night-guild/platform-app/pkg/connect/health/v1/healthv1connect"
 	"github.com/morning-night-guild/platform-app/pkg/trace"
 )
-
-type ConnectFactory interface {
-	Of(string) (*Connect, error)
-}
-
-type Connect struct {
-	Article articlev1connect.ArticleServiceClient
-	Health  healthv1connect.HealthServiceClient
-}
 
 func NewRequestWithTID[T any](ctx context.Context, msg *T) *connect.Request[T] {
 	req := connect.NewRequest(msg)
