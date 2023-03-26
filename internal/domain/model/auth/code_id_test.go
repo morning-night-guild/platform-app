9c86092cc8d2957d99d2f6ffb5a154f7ca1db5b1
takokun <takokun.778@gmail.com>
2023-03-27 00:04:39 +0900

    test(#101) 署名検証のテストを追加

--- internal/domain/model/auth/code_id_test.go
+++ internal/domain/model/auth/code_id_test.go
@@ -1,6 +1,10 @@
 package auth_test
 
 import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/base64"
 	"reflect"
 	"testing"
 
@@ -54,4 +58,77 @@ func TestNewCodeID(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestCodeIDVerify(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		signature  auth.Signature
+		privateKey *rsa.PrivateKey
+	}
+
+	key := generateKey(t)
+
+	tests := []struct {
+		name    string
+		cd      auth.CodeID
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "署名が検証できる",
+			cd:   auth.CodeID(uuid.MustParse("01234567-0123-0123-0123-0123456789ab")),
+			args: args{
+				signature:  auth.Signature(sign(t, key, "01234567-0123-0123-0123-0123456789ab")),
+				privateKey: key,
+			},
+			wantErr: false,
+		},
+		{
+			name: "署名が検証できない",
+			cd:   auth.CodeID(uuid.MustParse("01234567-0123-0123-0123-0123456789ab")),
+			args: args{
+				signature:  auth.Signature(""),
+				privateKey: key,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.cd.Verify(tt.args.signature, &tt.args.privateKey.PublicKey); (err != nil) != tt.wantErr {
+				t.Errorf("CodeID.Verify() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func sign(t *testing.T, prv *rsa.PrivateKey, code string) auth.Signature {
+	t.Helper()
+
+	h := crypto.Hash.New(crypto.SHA256)
+
+	h.Write([]byte(code))
+
+	hashed := h.Sum(nil)
+
+	signed, err := rsa.SignPSS(rand.Reader, prv, crypto.SHA256, hashed, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	return auth.Signature(base64.StdEncoding.EncodeToString(signed))
+}
+
+func generateKey(t *testing.T) *rsa.PrivateKey {
+	t.Helper()
+
+	prv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return prv
 }
