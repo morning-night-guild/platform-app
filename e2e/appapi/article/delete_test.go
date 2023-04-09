5ecae97f0b439e4a316ae9c5b56b5980cfee6e5a
Shitomo <cadaeibf@outlook.com>
2023-04-09 21:31:08 +0900

    test(#140) APIの記事削除機能のe2eテストを実装

---
+++ e2e/appapi/article/delete_test.go
@@ -0,0 +1,43 @@
+package article_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/morning-night-guild/platform-app/e2e/helper"
+)
+
+func TestAppAPIE2EArticleDelete(t *testing.T) {
+	t.Parallel()
+
+	size := uint32(5)
+
+	url := helper.GetAppAPIEndpoint(t)
+
+	t.Run("記事が削除できる", func(t *testing.T) {
+		t.Parallel()
+
+		db := helper.NewDatabase(t, helper.GetDSN(t))
+
+		ids := helper.NewIDs(t, int(size))
+
+		db.BulkInsertArticles(ids)
+
+		defer db.Close()
+
+		defer db.BulkDeleteArticles(ids)
+
+		client := helper.NewOpenAPIClient(t, url)
+
+		_, err := client.Client.V1ArticleDelete(context.Background(), ids[0])
+		if err != nil {
+			t.Fatalf("failed to delete article: %s", err)
+		}
+
+		article := db.SelectArticleByID(ids[0])
+
+		if article != nil {
+			t.Fatalf("article is not deleted")
+		}
+	})
+}
