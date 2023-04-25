96d658ab14f4c78026dbadeb55af09e5ff4ba997
takokun <takokun.778@gmail.com>
2023-04-25 23:11:13 +0900

    test(#186) 認証トークンの検証テストを追加

--- internal/domain/model/auth/auth_token_test.go
+++ internal/domain/model/auth/auth_token_test.go
@@ -34,3 +34,63 @@ func TestAuthTokenUserID(t *testing.T) {
 		})
 	}
 }
+
+func TestParseAuthTokenToken(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		userID        user.ID
+		encryptSecret auth.Secret
+		expiresIn     auth.ExpiresIn
+		decryptSecret auth.Secret
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "認証トークンが生成できる",
+			args: args{
+				userID:        user.ID(uuid.MustParse("01234567-0123-0123-0123-0123456789ab")),
+				encryptSecret: auth.Secret("secret"),
+				expiresIn:     auth.DefaultExpiresIn,
+				decryptSecret: auth.Secret("secret"),
+			},
+			wantErr: false,
+		},
+		{
+			name: "認証トークンの有効期限が切れている",
+			args: args{
+				userID:        user.ID(uuid.MustParse("01234567-0123-0123-0123-0123456789ab")),
+				encryptSecret: auth.Secret("secret"),
+				expiresIn:     auth.ExpiresIn(0),
+				decryptSecret: auth.Secret("secret"),
+			},
+			wantErr: true,
+		},
+		{
+			name: "暗号化と復号化のシークレットが異なる",
+			args: args{
+				userID:        user.ID(uuid.MustParse("01234567-0123-0123-0123-0123456789ab")),
+				encryptSecret: auth.Secret("sencrypt"),
+				expiresIn:     auth.DefaultExpiresIn,
+				decryptSecret: auth.Secret("decrypt"),
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := auth.GenerateAuthToken(tt.args.userID, tt.args.encryptSecret, tt.args.expiresIn)
+			if _, err := auth.ParseAuthToken(got.String(), tt.args.decryptSecret); (err != nil) != tt.wantErr {
+				t.Errorf("ParseAuthToken() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
